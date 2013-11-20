726f6468f5fb8f1f750029653cb3df1fdc0f19e0
Tej Chajed <tchajed+github@gmail.com>
2013-11-20 13:48:36 -0600

    Fix some bugs in experiment after testing
    also missing some things (no mpiexec, clis didn't pass output params)

--- experiment.py
+++ experiment.py
@@ -34,18 +34,28 @@ class GraphFile(object):
 
 class AbstractCli(object):
     program = None
-    def run(self):
-        args = [program]
+    is_mpi = False
+    def run_args(self):
+        args = []
+        if self.is_mpi:
+            args.append("mpiexec")
+        args.append(self.program)
         args.extend(self.args())
-        subprocess.call([str(arg) for arg in args])
+        return args
+    def run(self):
+        subprocess.call([str(arg) for arg in self.run_args()])
 
 
 class SampleCli(AbstractCli):
     program = "sample"
+    is_mpi = True
 
-    def __init__(self, algorithm, graph):
+    def __init__(self, algorithm, graph, output):
         self.algorithm = algorithm
-        self.graph - graph
+        self.graph = graph
+        self.output = output
+        if output.graph_format not in  ["tsv", "snap"]:
+            raise ValueError("sample cli always outputs in tsv/snap format")
     def args(self):
         args = self.graph.args()
         if self.algorithm["name"] == "forest_fire":
@@ -53,15 +63,18 @@ class SampleCli(AbstractCli):
             args.extend(["--prob", params["pf"]])
         else:
             raise ValueError("unknown algorithm " + self.algorithm["name"])
+        args.extend(["--output", self.output.prefix])
         return args
 
 
 class PartialResultsCli(AbstractCli):
     program = "partial_results"
+    is_mpi = True
 
-    def __init__(self, params, graph):
+    def __init__(self, params, graph, prefix):
         self.params = params
         self.graph = graph
+        self.prefix = prefix
     def args(self):
         args = self.graph.args()
         if self.params["engine"] not in ["synchronous"]:
@@ -70,16 +83,24 @@ class PartialResultsCli(AbstractCli):
         args.extend(["--tol", self.params["tolerance"]])
         args.extend(["--feature_period", self.params["feature_period"]])
         args.extend(["--max_vertices", self.params["accuracy_k"]])
+        args.extend(["--output", self.prefix])
+        return args
 
 
 class Features(object):
     def __init__(self, names, data):
-        self.names = names
-        self.data = data
+        self.names = list(names)
+        self.data = np.array(data)
     @classmethod
-    def from_file(cls, fname, sep):
+    def from_file(cls, fname):
         with open(fname, "r") as f:
             header_line = f.readline().strip()
+# try to identify separator, falling back to comma
+            sep = ","
+            for try_sep in ["|", "\t"]:
+                if header_line.find(try_sep) != -1:
+                    sep = try_sep
+                    break
             names = header_line.split(sep)
             data = []
             for l in f:
@@ -91,23 +112,32 @@ class Features(object):
     def __getitem__(self, features):
         indexes = []
         for feat in features:
-            idx = names.index(feat)
+            idx = self.names.index(feat)
             if idx == -1:
                 raise ValueError("unknown feature " + feat)
             indexes.append(idx)
-        filtered_names = [n for n in names if n in indexes]
-        return Features(filtered_names, np.delete(data, indexes, 1))
+        return Features(features, self.data[:, indexes])
 
-class Evaluate(object):
+
+class Evaluation(object):
     def __init__(self, params):
         self.algorithm = params["learn"]["algorithm"]
         self.features = params["features"]
         self.target_feature = params["target_feature"]
-    def base_model(self):
         name = self.algorithm["name"]
+        self.model = None
         if name == "SVR":
-            return SVR(**self.algorithm["parameters"])
+            model = SVR(**self.algorithm["parameters"])
         elif name == "ARDRegression":
-            return ARDRegression(**self.algorithm["parameters"])
+            model = ARDRegression(**self.algorithm["parameters"])
         raise ValueError("unknown sklearn regression algorithm " + name)
-
+        self.model = model
+    def train(self, train_fname):
+        features = Features.from_file(train_fname)
+        x_mat, y = features[self.features], features[target_feature]
+        self.model.fit(x_mat, y)
+    def test_error(self, test_fname):
+        features = Features.from_file(test_fname)
+        x_mat, y = features[self.features], features[target_feature]
+        y_hat = self.model.predict(x_mat)
+        return np.linalg.norm(y - y_hat)

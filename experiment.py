6c3300b08feda8c8e8c6387aa14659d44c7c2436
Tej Chajed <tchajed+github@gmail.com>
2013-11-20 11:54:31 -0600

    Add features class to manage named columnar data

--- experiment.py
+++ experiment.py
@@ -1,5 +1,7 @@
 # vim: et:tw=79:ts=4:sw=4
 import subprocess
+import yaml
+import numpy as np
 from sklearn.svm import SVR
 from sklearn.linear_model import ARDRegression
 
@@ -13,6 +15,12 @@ class Experiment(object):
         self.sample = o["sample"]
         self.run = o["run"]
         self.evaluate = o["evaluate"]
+    @classmethod
+    def from_yaml(cls, fname):
+        """ Load an experiment from a YAML file. """
+        with open(fname, "r") as f:
+            o = yaml.load(f)
+            return Experiment(o)
 
 
 class GraphFile(object):
@@ -63,6 +71,32 @@ class PartialResultsCli(AbstractCli):
         args.extend(["--feature_period", self.params["feature_period"]])
         args.extend(["--max_vertices", self.params["accuracy_k"]])
 
+
+class Features(object):
+    def __init__(self, names, data):
+        self.names = names
+        self.data = data
+    @classmethod
+    def from_file(cls, fname, sep):
+        with open(fname, "r") as f:
+            header_line = f.readline().strip()
+            names = header_line.split(sep)
+            data = []
+            for l in f:
+                line = l.strip()
+                data_row = [0.0 if v == "" else float(v) for v in
+                        line.split(sep)]
+                data.append(data_row)
+            return Features(names, data)
+    def __getitem__(self, features):
+        indexes = []
+        for feat in features:
+            idx = names.index(feat)
+            if idx == -1:
+                raise ValueError("unknown feature " + feat)
+            indexes.append(idx)
+        filtered_names = [n for n in names if n in indexes]
+        return Features(filtered_names, np.delete(data, indexes, 1))
 
 class Evaluate(object):
     def __init__(self, params):
